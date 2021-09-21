89424953fbfd1b7345bf770aa798cf3e432c3ced
Marcus Stewart <MMSStewart20@gmail.com>
2021-09-20 22:06:02 -0500

    Add files via upload

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -8,24 +8,40 @@ namespace GuessingGame
 {
     class Program
     {
+        private static void CheckForExit(string inputText)
+        {
+            if (inputText.Equals("Q"))
+            {
+                Console.WriteLine("You chose to exit. Goodbye!");
+                Environment.Exit(0);
+            }
+        }
+
         static void Main(string[] args)
         {
-            int theAnswer;
-            int playerGuess;
-            string playerInput;
-            string playerName;
+            int theAnswer,
+                playerGuess,
+                count = 0;
+            string playerInput,
+                playerName;
 
             Random r = new Random();
             theAnswer = r.Next(1, 21);
+            Console.WriteLine("Press Q to quit.\n");
+
+            // get player name
+            Console.WriteLine("What is your name: ");
+            playerName = Console.ReadLine();
+            CheckForExit(playerName);
+
+            //Console.WriteLine($"\n{theAnswer}");
 
             do
             {
-                Console.WriteLine("What is your name: ");
-                playerName = Console.ReadLine();
-
                 // get player input
                 Console.Write("\nEnter your guess (1-20): ");
                 playerInput = Console.ReadLine();
+                CheckForExit(playerInput);
 
                 //attempt to convert the string to a number
                 if (int.TryParse(playerInput, out playerGuess))
@@ -34,35 +50,54 @@ namespace GuessingGame
                     {
                         if (playerGuess == theAnswer)
                         {
-                            Console.WriteLine($"Hey {playerName}, {theAnswer} was the number. You win!");
-                            break;
+                            if (count == 0)
+                            {
+                                Console.WriteLine($"Hey {playerName}, you guessed correctly on your first try! {theAnswer} was the number. You win!");
+                                count++;
+                                Console.WriteLine($"Attempts: {count}\n");
+                                Console.WriteLine("The game is over, press Q to quit.");
+                                CheckForExit(playerInput);
+                            }
+                            else 
+                            {
+                                Console.WriteLine($"Hey {playerName}, {theAnswer} was the number. You win!");
+                                count++;
+                                Console.WriteLine($"Attempts: {count}\n");
+                                Console.WriteLine("The game is over, press Q to quit.");
+                                CheckForExit(playerInput);
+                            }
                         }
                         else
                         {
                             if (playerGuess > theAnswer)
                             {
                                 Console.WriteLine($"Hey {playerName}, your guess was too high, try again.\n");
+                                count++;
+                                Console.WriteLine($"Attempts: {count}\n");
+                                CheckForExit(playerInput);
                             }
                             else
                             {
                                 Console.WriteLine($"Hey {playerName}, your guess was too low, try again.\n");
+                                count++;
+                                Console.WriteLine($"Attempts: {count}\n");
+                                CheckForExit(playerInput);
                             }
                         }
                     }
-                    else 
+                    else
                     {
                         Console.WriteLine($"Hey {playerName}, {playerGuess} is not in the range. Try again.\n");
+                        CheckForExit(playerInput);
                     }
                 }
                 else
                 {
                     Console.WriteLine($"Hey {playerName}, you entered {playerGuess}, which is not a number!\n");
+                    CheckForExit(playerInput);
                 }
 
             } while (true);
-
-            Console.WriteLine("Press any key to quit.");
-            Console.ReadKey();
         }
     }
 }
