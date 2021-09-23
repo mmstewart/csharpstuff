759a1c396d4eaeac50d5369a4ad041e0aa9252fe
Marcus Stewart <MMSStewart20@gmail.com>
2021-09-23 13:54:32 -0500

    Add files via upload

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -8,45 +8,43 @@ namespace GuessingGame
 {
     class Program
     {
-        private static void CheckForExit(string inputText)
+        private static bool CheckForExit(string inputText)
         {
-            if (inputText.Equals("Q"))
+            var isPlaying = !inputText.Equals("Q");
+            if (!isPlaying)
             {
                 Console.WriteLine("You chose to exit. Goodbye!");
                 Environment.Exit(0);
             }
+
+            return isPlaying;
         }
 
         static void Main(string[] args)
         {
-            int theAnswer,
-                playerGuess,
-                count = 0;
-            string playerInput,
-                playerName;
+            int count = 0;
 
             Random r = new Random();
-            theAnswer = r.Next(1, 21);
+            var theAnswer = r.Next(1, 21);
             Console.WriteLine("Press Q to quit.\n");
 
             // get player name
             Console.WriteLine("What is your name: ");
-            playerName = Console.ReadLine();
+            var playerName = Console.ReadLine();
             CheckForExit(playerName);
-
-
 
             Console.WriteLine($"\n{theAnswer}");
 
+            bool isPlaying;
             do
             {
                 // get player input
                 Console.Write("\nEnter your guess (1-20): ");
-                playerInput = Console.ReadLine();
+                var playerInput = Console.ReadLine();
                 CheckForExit(playerInput);
 
                 //attempt to convert the string to a number
-                if (int.TryParse(playerInput, out playerGuess))
+                if (int.TryParse(playerInput, out var playerGuess))
                 {
                     if (playerGuess >= 1 && playerGuess <= 20)
                     {
@@ -54,23 +52,21 @@ namespace GuessingGame
                         {
                             if (count == 0)
                             {
-                                
-                                
-
                                 Console.WriteLine($"Hey {playerName}, you guessed correctly on your first try! {theAnswer} was the number. You win!", Console.ForegroundColor = ConsoleColor.Green);
                                 count++;
                                 Console.WriteLine($"Attempts: {count}\n", Console.ForegroundColor = ConsoleColor.Gray);
                                 Console.WriteLine("The game is over, press Q to quit.");
-                                CheckForExit(playerInput);
                             }
-                            else 
+                            else
                             {
                                 Console.WriteLine($"Hey {playerName}, {theAnswer} was the number. You win!", Console.ForegroundColor = ConsoleColor.Green);
                                 count++;
                                 Console.WriteLine($"Attempts: {count}\n", Console.ForegroundColor = ConsoleColor.Gray);
                                 Console.WriteLine("The game is over, press Q to quit.");
-                                CheckForExit(playerInput);
                             }
+
+                            Console.ReadKey();
+                            isPlaying = false;
                         }
                         else
                         {
@@ -79,14 +75,14 @@ namespace GuessingGame
                                 Console.WriteLine($"Hey {playerName}, your guess was too high, try again.\n", Console.ForegroundColor = ConsoleColor.Red);
                                 count++;
                                 Console.WriteLine($"Attempts: {count}\n", Console.ForegroundColor = ConsoleColor.Gray);
-                                CheckForExit(playerInput);
+                                isPlaying = CheckForExit(playerInput);
                             }
                             else
                             {
                                 Console.WriteLine($"Hey {playerName}, your guess was too low, try again.\n", Console.ForegroundColor = ConsoleColor.Red);
                                 count++;
                                 Console.WriteLine($"Attempts: {count}\n", Console.ForegroundColor = ConsoleColor.Gray);
-                                CheckForExit(playerInput);
+                                isPlaying = CheckForExit(playerInput);
                             }
                         }
                     }
@@ -94,17 +90,16 @@ namespace GuessingGame
                     {
                         Console.WriteLine($"Hey {playerName}, {playerGuess} is not in the range. Try again.\n", Console.ForegroundColor = ConsoleColor.Red);
                         Console.ForegroundColor = ConsoleColor.Gray;
-                        CheckForExit(playerInput);
+                        isPlaying = CheckForExit(playerInput);
                     }
                 }
                 else
                 {
                     Console.WriteLine($"Hey {playerName}, you entered {playerGuess}, which is not a number!\n", Console.ForegroundColor = ConsoleColor.Red);
                     Console.ForegroundColor = ConsoleColor.Gray;
-                    CheckForExit(playerInput);
+                    isPlaying = CheckForExit(playerInput);
                 }
-
-            } while (true);
+            } while (isPlaying);
         }
     }
 }
