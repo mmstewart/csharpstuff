8436521f7576968f59c92dc6a6130bfefbc2af5e
Marcus Stewart <MMSStewart20@gmail.com>
2021-10-13 22:14:11 -0500

    Add files via upload

---
+++ Warmups/Warmups.BLL/Conditionals.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Linq;
+
+namespace Warmups.BLL
+{
+    public class Conditionals
+    {
+        public bool AreWeInTrouble(bool aSmile, bool bSmile)
+        {
+            return aSmile == bSmile;
+        }
+
+        public bool CanSleepIn(bool isWeekday, bool isVacation)
+        {
+            return !isWeekday || isVacation;
+        }
+
+        public int SumDouble(int a, int b)
+        {
+            return a == b ? 2 * (a + b) : (a + b);
+        }
+
+        public int Diff21(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ParrotTrouble(bool isTalking, int hour)
+        {
+            return isTalking && hour == 6;
+        }
+
+        public bool Makes10(int a, int b)
+        {
+            return a + b == 10 || (a == 10 || b == 10);
+        }
+
+        public bool NearHundred(int n)
+        {
+            return Enumerable.Range(90, 110).Contains(n);
+        }
+
+        public bool PosNeg(int a, int b, bool negative)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NotString(string s)
+        {
+            return "not " + s;
+        }
+
+        public string MissingChar(string str, int n)
+        {
+            return str.Remove(n, 1);
+        }
+
+        public string FrontBack(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Front3(string str)
+        {
+            if (str.Length < 3)
+                return string.Concat(Enumerable.Repeat(str, 3));
+            return string.Concat(Enumerable.Repeat(str.Remove(3), 3));
+        }
+
+        public string BackAround(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Multiple3or5(int number)
+        {
+            return number % 3 == 0 || number % 5 == 0;
+        }
+
+        public bool StartHi(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IcyHot(int temp1, int temp2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Between10and20(int a, int b)
+        {
+            return a >= 10 && a <= 20 || b >= 10 && b <= 20;
+        }
+
+        public bool HasTeen(int a, int b, int c)
+        {
+            return a >= 13 && a <= 19 ||
+                b >= 13 && b <= 19 ||
+                c >= 13 && c <= 19;
+        }
+
+        public bool SoAlone(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RemoveDel(string str)
+        {
+            return str.Replace("del", "");
+        }
+
+        public bool IxStart(string str)
+        {
+            return str.Contains("ix");
+        }
+
+        public string StartOz(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Max(int a, int b, int c)
+        {
+            return Math.Max(a, Math.Max(b, c));
+        }
+
+        public int Closer(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GotE(string str)
+        {
+            return str.Count(i => i == 'e') % 2 != 0;
+        }
+
+        public string EndUp(string str)
+        {
+            return str.Length < 3 ? str.ToUpper() : str.Remove(str.Length - 3) + str.Substring(str.Length - 3).ToUpper();
+        }
+
+        public string EveryNth(string str, int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Warmups/Warmups.BLL/Strings.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Linq;
+
+namespace Warmups.BLL
+{
+    public class Strings
+    {
+
+        public string SayHi(string name)
+        {
+            return "Hello " + name + "!";
+        }
+
+        public string Abba(string a, string b)
+        {
+            return a + b + b + a;
+        }
+
+        public string MakeTags(string tag, string content)
+        {
+            return "<" + tag + ">" + content + "</" + tag + ">";
+        }
+
+        public string InsertWord(string container, string word)
+        {
+            return container.Substring(0, 2) + word + container.Substring(2);
+        }
+
+        public string MultipleEndings(string str)
+        {
+            return string.Concat(Enumerable.Repeat((str.Substring(str.Length - 2)), 3));
+        }
+
+        public string FirstHalf(string str)
+        {
+            return str.Substring(0, str.Length / 2);
+        }
+
+        public string TrimOne(string str)
+        {
+            return str.Substring(1, str.Length - 2);
+        }
+
+        public string LongInMiddle(string a, string b)
+        {
+            return a.Length > b.Length ? b + a + b : a + b + a;
+        }
+
+        public string RotateLeft2(string str)
+        {
+            if (str.Length < 3)
+                return str;
+
+            return str.Substring(2) + str.Substring(0, 2);
+        }
+
+        public string RotateRight2(string str)
+        {
+            if (str.Length < 3)
+                return str;
+
+            return str.Substring(str.Length - 2, 2) + str.Substring(0, str.Length - 2);
+        }
+
+        public string TakeOne(string str, bool fromFront)
+        {
+            //if (str. == fromFront)
+            //    return str.Substring(0, 1);
+            //return str.Substring(str.Length - 1, 1);
+            throw new NotImplementedException();
+        }
+
+        public string MiddleTwo(string str)
+        {
+            var offset = str.Length % 2 == 0 ? 1 : 0;
+            return str.Substring(str.Length / 2 - offset, offset + 1);
+        }
+
+        public bool EndsWithLy(string str)
+        {
+            return str.Contains("ly") ? true : false;
+        }
+
+        public string FrontAndBack(string str, int n)
+        {
+            return str.Substring(0, n) + str.Substring(str.Length - n);
+        }
+
+        public string TakeTwoFromPosition(string str, int n)
+        {
+            return n == 2 ? str.Substring(2) : str.Substring(0, 2);
+        }
+
+        public bool HasBad(string str)
+        {
+            if (str.EndsWith("xx"))
+                if (!str.StartsWith("xx"))
+                    return true;
+
+            return false;
+        }
+
+        public string AtFirst(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string LastChars(string a, string b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConCat(string a, string b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SwapLast(string str)
+        {
+            if (str.Length < 2)
+                return str;
+
+            return str.Substring(0, str.Length - 2) + str.Substring(str.Length - 1) + str.Substring(str.Length - 2, 1);
+        }
+
+        public bool FrontAgain(string str)
+        {
+            if (str.StartsWith("ed"))
+            {
+                if (str.EndsWith("ed"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public string MinCat(string a, string b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TweakFront(string str)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string StripX(string str)
+        {
+            if (str.Equals("x"))
+                return String.Empty;
+
+            if (str.StartsWith("x") && str.EndsWith("x"))
+            {
+                return str.Substring(1, str.Length - 2);
+            }
+            else if (str.StartsWith("x"))
+            {
+                return str.Substring(1, str.Length - 1);
+            }
+            else if (str.EndsWith("x"))
+            {
+                return str.Substring(0, str.Length - 1);
+            }
+
+            return str;
+        }
+    }
+}
